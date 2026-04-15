ea123a315888a6cce70e24bd2646b5b6734e59ac
Iremide <IAdewumi@smail.pembinatrails.ca>
2026-04-15 12:04:09 -0500

    Bug fixes and general improvements

--- TBAitest.sh
+++ TBAitest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 shopt -s nocasematch
 
+#Variables 
 player_hp=100
 player_energy=100
 
@@ -52,7 +53,7 @@ energy_menu(){
 	2. Battle Heal - Recover 50% of the damage you take as
 	health. 
 
-	3.Sharpness - Double sword damage for a turn
+	3.Sharpness - Double sword damage for a turn. You take a bonus turn immediately after.
 	
 
 	Type back to return" nrg
@@ -61,69 +62,97 @@ energy_menu(){
 	1)
 		read -p "Thorns costs 20 energy. Are you sure you want to enchant?
 		" thorns
-			if [[ $thorns == "yes" ]]; then
+			if [[ $thorns == "yes" && player_energy -ge 20 ]]; then
 				echo "Enchanted"
+				turn_taken=true
+				
 			else
-				echo "exiting"
+				echo "Not enough energy or exited interface."
+				
 			fi
 
 		;;
 	2)
 		read -p "Battle Heal costs 30 energy. Are you sure you want to enchant?
 		" bheal
-			if [[ $bheal == "yes" ]]; then
+			if [[ $bheal == "yes" && player_energy -ge 30  ]]; then
 				echo "Enchanted"
+				turn_taken=true
+				
 			fi
 		;;
 	3)	read -p "Sharpness costs 40 energy. Are you sure you want to enchant?
 		" sharpness
-			if [[ $sharpness == "yes" ]]; then
+			if [[ $sharpness == "yes" && player_energy -ge 20 ]]; then
+				player_energy = $((player_energy-= 0))
 				echo "Enchanted"
+				turn_taken=false
+				
 			fi
 		;;
+	*)  echo "returning to main screen"
+			
+			;;
 
 	esac
 }
 
 
+ ai_turn() {
+ 	clear
+
+ }
+
 AI_encounter(){
 while ((player_hp > 0 && ai_hp > 0)); do
-	read -p "Action to take 
-	1. Fight
-	2. Defend
-	3. Energy moves" choice
-	case $choice in
-		1)
-		  	fight_menu
-		  	;;
-		2)
-			defend_menu
-			;;
-		3)
-			energy_menu
-			;;
+	
+	turn_taken=false
+
+	while [[ $turn_taken == false ]]; do
+		read -p "Action to take 
+		1. Fight
+		2. Defend
+		3. Energy moves
+		" choice
+	
+		case $choice in
+			1)
+		  		fight_menu
+		  		;;
+			2)
+				defend_menu
+				;;
+			3)
+				energy_menu
+				;;
 		
-		*)
-			echo -e "Do I need to translate to Bahasa Indonesia for you???"
-			;;
+			*)
+				echo -e "Do I need to translate to Bahasa Indonesia for you???"
+				;;
 
 
-	esac
+		esac
+	done
+	ai_turn
 done
 }
 	
 echo "Today, you will fight an AI with a pre-determined loadout."
 sleep 2
-read -p "You ready (yes/no)?" combat
-if [[ $combat == "yes" || "y" ]]; then
-	echo "Let us begin."
-	sleep 2
-	AI_encounter
+while true; do
+	read -p "You ready (yes/no)?" combat
+	if [[ $combat == "yes" || $combat == "y" ]]; then
+		echo "Let us begin."
+		sleep 2
+		AI_encounter
+		break
 
-elif [[ $combat == "no" || "n" ]]; then
-	echo "Let us begin."
-	sleep 2
-	echo -e "Please retry."
-	exit 0
-fi
+	elif [[ $combat == "no" || "n" ]]; then
+		echo "get out"
+		sleep 2
+		exit 0
 
+	else
+		echo "hm?"
+	fi
+done
