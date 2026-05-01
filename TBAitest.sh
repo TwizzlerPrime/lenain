a8558f9ea70b703d6b082c6e275763b38625056d
Iremide <ixdem353@gmail.com>
2026-05-01 12:01:57 -0500

    Adding more parameters to apply_effect to include more buffs in other calculations

--- TBAitest.sh
+++ TBAitest.sh
@@ -23,9 +23,11 @@ AI_defense=0
 #Effect functions
 apply_effect() {
 	local effect=$1
-	local duration=$2
+	local type=$2
+	local duration=$3
+	local value=$4
 
-	player_effects+=("$effect:$duration")
+	player_effects+=("$effect:$type:$duration:$value")
 }
 
 
@@ -34,12 +36,16 @@ update_effects() {
 
 	for effect in "${player_effects[@]}"; do
 		name="${effect%%:*}"
+		temp="${effect#*:}"
+		type="${temp%%:*}"
 		turns="${effect##*:}"
 
 		((turns--))
 
 		if ((turns > 0)); then
 			new_effects+=("$name:$turns")
+
+
 		else
 			echo "$name has worn off."
 		fi
@@ -72,7 +78,7 @@ calculate_damage() {
 on_player_hit() {
 	local damage_taken=$1
 	if has_effect "Thorns"; then
-		local reflect$((damage_taken * 25 / 100))
+		local reflect=$((damage_taken * 25 / 100))
 		echo "Reflect $reflect damage with thorns."
 		AI_hp=$((AI_hp - reflect))
 	fi
@@ -173,11 +179,19 @@ energy_menu(){
 	1)
 		read -p "Thorns costs 20 energy. Are you sure you want to enchant?
 		" thorns
+
+			if has_effect "Thorns"; then
+				echo "a little too sharp ay??"
+				sleep 1
+				return 0
+			fi
+
 			if [[ $thorns == "yes" && $player_energy -ge 20 ]]; then
 				player_energy=$((player_energy-=20))
 				echo "Enchanted"
 				apply_effect "Thorns" 1
 				turn_taken=true
+				update_effects
 				
 			else
 				echo "Not enough energy or exited interface."
@@ -188,23 +202,38 @@ energy_menu(){
 	2)
 		read -p "Battle Heal costs 30 energy. Are you sure you want to enchant?
 		" bheal
+
+			if has_effect "Lifesteal"; then
+				echo "a little too sharp ay??"
+				sleep 1
+				return 0 
+			fi
+
 			if [[ $bheal == "yes" && $player_energy -ge 30  ]]; then
 				player_energy=$((player_energy - 30))
 				echo "Enchanted"
-				apply_effect "Lifesteal" 2
+				apply_effect "Lifesteal" "healthbuff" 2 15
 				turn_taken=true
+				
 				
 			fi
 		;;
 	3)	read -p "Sharpness costs 40 energy. Are you sure you want to enchant?
 		" sharpness
+			if has_effect "Sharpness"; then
+				echo "a little too sharp ay??"
+				sleep 1
+				return 0
+			fi
+
 			if [[ $sharpness == "yes" && $player_energy -ge 40 ]]; then
 				player_energy=$((player_energy - 40))
 				damage_buff=$((damage_buff + 10))
 				echo "Enchanted"
-				apply_effect "Sharpness" 1
+				apply_effect "Sharpness" "damagebuff" 1 10
 				turn_taken=false
 				sleep 2
+				
 			fi
 		;;
 	*)  echo "returning to main screen"
@@ -298,6 +327,7 @@ while ((player_hp > 0 && AI_hp > 0)); do
 		1. Fight
 		2. Defend
 		3. Energy moves
+		4. Display current buffs
 		" choice
 	
 		case $choice in
@@ -311,70 +341,24 @@ while ((player_hp > 0 && AI_hp > 0)); do
 				energy_menu
 				;;
 		
+			4)	
+				echo -e "Current status effects:"
+				if ((${#player_effects[@]} == 0)); then
+					echo "None"
+				else
+					for effect in "${player_effects[@]}"; do
+						echo " - $effect"
+					done
+				fi
+				;;
 			*)
 				echo -e "Do I need to translate to Bahasa Indonesia for you???"
 				;;
 
 
 		esac
-	done
-	AI_turn
-	AI_turn_taken=false
-done
-}
-	
-echo "Today, you will fight an AI with a pre-determined loadout."
-sleep 2
-while true; do
-	read -p "You ready (yes/no)?" combat
-	if [[ $combat == "yes" || $combat == "y" ]]; then
-		echo "Let us begin."
-		sleep 2
-		AI_encounter
-		break
-
-	elif [[ $combat == "no" || "n" ]]; then
-		echo "get out"
-		sleep 2
-		exit 0
-
-	else
-		echo "hm?"
-	fi
-done
-
-
-AI_encounter(){
-while ((player_hp > 0 && AI_hp > 0)); do
-	
-	turn_taken=false
-
-	while [[ $turn_taken == false ]]; do
-		read -p "Action to take 
-		1. Fight
-		2. Defend
-		3. Energy moves
-		" choice
-	
-		case $choice in
-			1)
-		  		fight_menu
-		  		;;
-			2)
-				defend_menu
-				;;
-			3)
-				energy_menu
-				;;
 		
-			*)
-				echo -e "Do I need to translate to Bahasa Indonesia for you???"
-				;;
-
-
-		esac
 	done
-	update_effects
 	AI_turn
 	AI_turn_taken=false
 	update_effects
@@ -400,3 +384,6 @@ while true; do
 		echo "hm?"
 	fi
 done
+
+
+
